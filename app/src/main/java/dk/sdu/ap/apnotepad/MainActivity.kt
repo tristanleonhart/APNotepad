56da8aed3565c38b1e5d56f470339b2e30f13ae9
Etienne Gubler <gubleet1@students.zhaw.ch>
2021-05-01 15:44:32 +0200

    Ask for file access permission on import

--- app/src/main/java/dk/sdu/ap/apnotepad/MainActivity.kt
+++ app/src/main/java/dk/sdu/ap/apnotepad/MainActivity.kt
@@ -1,13 +1,16 @@
 package dk.sdu.ap.apnotepad
 
+import android.Manifest
 import android.content.ClipData
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextSwitcher
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
@@ -68,8 +71,12 @@ class MainActivity : AppCompatActivity(), FolderItemRecyclerViewAdapter.ItemClic
             // create a folder or subfolder
             FolderEditDialog().show(supportFragmentManager, null)
             return true
+        } else if (item.itemId == R.id.import_notes) {
+            // import notes and folders from a json file
+            importData()
+            return true
         } else if (item.itemId == R.id.export_notes) {
-            // export all notes and folders as json file
+            // export notes and folders to a json file
             exportData()
             return true
         } else if (item.itemId == android.R.id.home) {
@@ -308,6 +315,37 @@ class MainActivity : AppCompatActivity(), FolderItemRecyclerViewAdapter.ItemClic
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         } else {
             supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        }
+    }
+
+    private fun importData() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    0
+                )
+                return
+            }
+        }
+        browseFiles()
+    }
+
+    private fun browseFiles() {
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            browseFiles()
+        } else {
+            Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
         }
     }
 
